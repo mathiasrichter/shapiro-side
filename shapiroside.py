c99a29301f15ca8a4299fa40a64f82deed9aebb1
Mathias Richter <mathias.richter@gmail.com>
2023-01-04 16:00:13 +0100

    Cleaned up class hierarchy, moved to prepared queries.

--- shapiroside.py
+++ shapiroside.py
@@ -1,4 +1,5 @@
 from rdflib import Graph
+from rdflib.plugins.sparql import prepareQuery
 from abc import ABC, abstractmethod
 from pyspark.sql import SparkSession
 
@@ -9,7 +10,34 @@ class Sidecar(ABC):
     the sidecar in a way compliant with the model.
     The Sidecar has an abstract method that subclasses need to implement as a notion of running this particular sidecar
     by interpreting the specified description (available the data IRI) in the context of the
-    semantic model (available at the model IRI)."""
+    semantic model (available at the model IRI). 
+    This class assumes a model as described at ./models/DataProduct.ttl"""
+    
+    INPUT_PORT_QUERY = prepareQuery("""
+            PREFIX dtp: <http://example.org/DataProduct/> 
+            SELECT DISTINCT ?inputPort
+            WHERE { 
+                ?inputPortClass rdfs:subClassOf* dtp:InputPort. 
+                ?inputPort rdf:type ?inputPortClass. 
+            }
+        """)
+    
+    OUTPUT_PORT_QUERY = prepareQuery("""
+            PREFIX dtp: <http://example.org/DataProduct/>
+            SELECT DISTINCT ?outputPort
+            WHERE { 
+                ?outputPortClass rdfs:subClassOf* dtp:OutputPort. 
+                ?outputPort rdf:type ?outputPortClass. 
+            }
+        """)
+    
+    FILE_INPUT_PORT_QUERY = prepareQuery("""
+            PREFIX dtp: <http://example.org/DataProduct/>
+            SELECT DISTINCT ?inputPort
+            WHERE { 
+                ?inputPort rdf:type dtp:FileInputPort. 
+            }
+        """)                                         
     
     def __init__(self, model_iri:str, data_iri:str):
         """Initialize this sidecar.
@@ -23,71 +51,36 @@ class Sidecar(ABC):
         self.graph = Graph().parse(model_iri)
         self.graph.parse(self.data_iri) # merge all into the same graph
         
-    @abstractmethod
-    def run(self):
-        """Performs sidecar actions by interpreting relevant parts of the
-        data graph describing the actual data product associated with this sidecar instance.
-        """
+    def get_instances_of(self, class_iri:str):
         pass
-
-class DataProduct(Sidecar):
-    """A sidecar representing providing all information about a data product following the conceptual model described under
-    ./models/DataProduct.ttl"""
-    
-    def __init__(self, model_iri:str, data_iri:str):
-        """Initialize this sidecar.
-
-        Args:
-            model_iri (_type_): An IRI pointing to a semantic model defining the format that the data graph must have.
-            data_iri (_type_): An IRI pointing to the actual description of a data product in compliance with the model.
-        """
-        super().__init__(model_iri, data_iri)
-    
+        
     def get_input_ports(self):
-        query = """
-            SELECT DISTINCT ?inputPort
-            WHERE { 
-                ?inputPortClass rdfs:subClassOf* dtp:InputPort. 
-                ?inputPort rdf:type ?inputPortClass. 
-            }
-        """
-        result = self.graph.query(query)
+        result = self.graph.query(self.INPUT_PORT_QUERY)
         inputPorts = []
         for row in result:
             inputPorts.append(row.inputPort)
         return inputPorts
     
     def get_output_ports(self):
-        query = """
-            SELECT DISTINCT ?inputPort
-            WHERE { 
-                ?inputPortClass rdfs:subClassOf* dtp:OutputPort. 
-                ?inputPort rdf:type ?inputPortClass. 
-            }
-        """
-        result = self.graph.query(query)
-        inputPorts = []
+        result = self.graph.query(self.OUTPUT_PORT_QUERY)
+        outputPorts = []
         for row in result:
-            inputPorts.append(row.inputPort)
-        return inputPorts
+            outputPorts.append(row.outputPort)
+        return outputPorts
     
     def get_file_input_ports(self):
-        query = """
-            SELECT DISTINCT ?inputPort
-            WHERE { 
-                ?inputPort rdf:type dtp:FileInputPort. 
-            }
-        """
-        result = self.graph.query(query)
+        result = self.graph.query(self.FILE_INPUT_PORT_QUERY)
         inputPorts = []
         for row in result:
             inputPorts.append(row.inputPort)
         return inputPorts
-    
+
+    @abstractmethod
     def run(self):
-        print(self.get_input_ports())
-        print(self.get_output_ports())
-        print(self.get_file_input_ports())
+        """Performs sidecar actions by interpreting relevant parts of the
+        data graph describing the actual data product associated with this sidecar instance.
+        """
+        pass
  
 class SparkSidecar(Sidecar):
     """An abstract sidecar that uses Spark to process data.
@@ -105,16 +98,4 @@ class FileInputPort(SparkSidecar):
         super().__init__(model_iri, data_iri, sparkSession)
         
     def run(self):
-        fileinputs_query = """
-        SELECT DISTINCT ?p ?i ?v ?d
-        WHERE {
-            ?p dtp:hasInputPort ?i .
-            ?i rdf:type dtp:FileInputPort .
-            ?i ?v ?d .
-        }"""
-
-        qres = self.data.query(fileinputs_query)
-        for row in qres:
-            print(f"{row.p} has file input port {row.i} has attribute {row.v} with value {row.d}")   
-
-
+        pass
