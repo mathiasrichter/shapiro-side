00f072f270b1f6a91f7ed0b86895eb2161ad5edf
Mathias Richter <mathias.richter@gmail.com>
2023-01-08 13:23:45 +0100

    Continued work on file input port sidecar (still work in progress).

--- shapiroside.py
+++ shapiroside.py
@@ -4,6 +4,7 @@ from rdflib import URIRef, BNode
 from abc import ABC, abstractmethod
 from pyspark.sql import SparkSession
 from urllib.parse import urlparse
+import re
 
 class Sidecar(ABC):
     """A Sidecar is the abstract superclass from which all sidecars descent.
@@ -27,14 +28,14 @@ class Sidecar(ABC):
         self.data_iri = data_iri
         self.graph = Graph().parse(model_iri)
         self.graph.parse(self.data_iri) # merge all into the same graph
-        dptNamespace = None
+        self.dptNamespace = None
         for n in self.graph.namespace_manager.namespaces():
             ns = str(n[1])
             if ns.endswith("/DataProduct/") or ns.endswith("/DataProduct#"):
-                dptNamespace = ns
-        if dptNamespace is None:
+                self.dptNamespace = ns
+        if self.dptNamespace is None:
             raise Exception("Could not identify namespace of model for DataProduct.")
-        self.prepare_queries(dptNamespace)         
+        self.prepare_queries(self.dptNamespace)         
         
     def prepare_queries(self, dataProductNamespace:str):
         """Prepare all SPARQL queries needed to interpret the dataproduct description.
@@ -82,12 +83,22 @@ class Sidecar(ABC):
         return properties        
         
     @abstractmethod
-    def run(self):
+    def start(self):
         """Performs sidecar actions by interpreting relevant parts of the
         data graph describing the actual data product associated with this sidecar instance.
+        These actions can be one-off (so start finishes after performing all actions), or continuous
+        (ie. start runs a loop or even spawns multiple threads for repeating and concurrent action). 
+        In case of continuous action the sidecar can be stopped using the stop method.
         """
         pass
- 
+    
+    @abstractmethod
+    def stop(self):
+        """Halts all activity of this sidecar (if it has previously been started and is in 
+        some sort of a running state by whatever definition).
+        """
+        pass 
+    
 class SparkSidecar(Sidecar):
     """An abstract sidecar that uses Spark to process data.
     """
@@ -97,11 +108,69 @@ class SparkSidecar(Sidecar):
         self.sparkSession = sparkSession
 
 
-class FileInputPort(SparkSidecar):
-    """This sidecar implementation operates a file-based input port."""
+class FileInputPortSidecar(SparkSidecar):
+    """This sidecar implementation operates the file-based input port as declared in the desciption under data_iri 
+    and shares this data via a SparkSession within the architecture quantum of the data product declared under data_iri."""
     
-    def __init__(self, model_iri:str, data_iri:str, sparkSession:SparkSession):
+    SCHEDULE_PROP           = "/schedule"
+    PATH_PROP               = "http://www.w3.org/ns/dcat#endpointURL"   
+    NAME_PATTERN_PROP       = "/namePattern"
+    FORMAT_PROP             = "/format"
+    FORMAT_CSV              = "/CSV"
+    FORMAT_JSON             = "/JSON"
+    
+    def __init__(self, model_iri:str, data_iri:str, port_iri:str, sparkSession:SparkSession):
         super().__init__(model_iri, data_iri, sparkSession)
+        self.port_iri = port_iri
+        self.configure()
         
-    def run(self):
+    def configure(self):
+        props = self.get_properties_of_instance(self.port_iri)
+        self.configure_format(props)
+        self.configure_name(props)
+        self.configure_path(props)
+        self.configure_schedule(props)
+    
+    def configure_format(self, properties:dict):
+        self.format = None
+        for p in properties:
+            if str(p).endswith(self.FORMAT_PROP):
+                if str(properties[p].endswith(self.FORMAT_CSV)):
+                    self.format = self.FORMAT_CSV
+                elif str(properties[p].endswith(self.FORMAT_JSON)):
+                    self.format = self.FORMAT_JSON
+                else:
+                    raise Exception("Unknown file format: {}".format(str(p)))
+        if self.format is None:
+            raise Exception("No format specified in properties for file input port {}.".format(self.port_iri))        
+            
+    def configure_name(self, properties:dict):
+        self.namePattern = None
+        for p in properties:
+            if str(p).endswith(self.NAME_PATTERN_PROP):
+                self.namePattern = str(properties[p])
+        if self.namePattern is None:
+            raise Exception("No name pattern specified in properties for file input port {}.".format(self.port_iri))                    
+
+    def configure_path(self, properties:dict):
+        self.path = None
+        for p in properties:
+            if str(p) == (self.PATH_PROP):
+                path = str(properties[p])
+                self.path = urlparse(path).path                
+        if self.path is None:
+            raise Exception("No path/endpointURL specified in properties for file input port {}.".format(self.port_iri))                    
+
+    def configure_schedule(self, properties:dict):
+        self.schedule = None
+        for p in properties:
+            if str(p) == (self.SCHEDULE_PROP):
+                pass
+        if self.schedule is None:
+            raise Exception("No schedule specified in properties for file input port {}.".format(self.port_iri))                    
+
+    def start(self):
+        pass
+    
+    def stop(self):
         pass
